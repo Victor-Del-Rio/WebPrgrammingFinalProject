7d3bd8999548e21b2b6f3c77b8778911d2028cb9
j-hender <48694706+j-hender@users.noreply.github.com>
2021-12-02 21:33:32 -0600

    Update signup.php

--- signup.php
+++ signup.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1"> 
-  <title>CSCI Lab Login</title>
+  <title>CSCI Sign Up Form</title>
 </head>
 <style type="text/css">.error{color: red;}</style>
 <style>
